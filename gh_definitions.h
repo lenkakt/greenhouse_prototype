146d92edb2974f5cabfa4508310a9e79b5466bd9
lenka.koskova.triskova <lenka.koskova.triskova@tul.cz>
2025-09-03 18:33:42 +0200

    Main state flow ready.

---
+++ gh_definitions.h
@@ -0,0 +1,39 @@
+// Greenhouse main internal states
+#define GH_STATE_SLEEP 1              //Device in sleep mode to save energy
+#define GH_STATE_ON 2                 //Device in normal operation 
+#define GH_STATE_UI 3                 //Device interacting with user
+#define GH_STATE_INIT 4               //Device is initializing
+#define GH_STATE_CLOSING 5            //Device is switching off, closing connections etc. 
+#define GH_STATE_ERROR 6              //Device faces some error; restart needed
+
+#define GH_ACT_IN_TEMP 1
+#define GH_ACT_IN_HUMIDITY 2
+#define GH_ACT_OUT_WATER 3
+#define GH_ACT_OUT_DOOR 4
+#define GH_ACT_TELEMETRY 5
+#define GH_ACT_UI_LOCAL 6
+#define GH_ACT_UI_BT 7
+#define GH_ACT_UI_NET 8
+#define GH_ACT_CLOUD 9
+
+#define GH_DELAY_SHORT 1000
+#define GH_DELAY_LONG 3000
+
+#define GH_MAX_LOOP 5
+
+#define GH_DEBUG_OFF 0 //No debug prints
+#define GH_DEBUG_ON 1 // Debug levels
+#define GH_DEBUG_STRING "GH DEBUG: "
+#define GH_ERR_STRING "GH ERROR: "
+
+
+#define GH_ERR_NOERR 0 //No error detected, normal operation
+#define GH_ERR_NOERR_STR GH_ERR_STRING "No error, normal operation.\n"
+#define GH_ERR_NOINIT 1 //Device starting init in wrong state
+#define GH_ERR_NOINIT_STR GH_ERR_STRING "Device in wrong state at init, restarting.\n"
+#define GH_ERR_UNKNOWN_STATE 2 //Device in undefined state (restart needed).
+#define GH_ERR_UNKNOWN_STATE_STR GH_ERR_STRING "Device in undefined state, restarting.\n"
+#define GH_ERR_MAX_COUNT 3 // Device reached the max allowed number of cycles. 
+#define GH_ERR_MAX_COUNT_STR GH_ERR_STRING "Device reached max allowed number of cycles. Restarting.\n"
+
+
--- greenhouse_prototype.ino
+++ greenhouse_prototype.ino
@@ -2,36 +2,8 @@
 // Author: Lenka Koskova Triskova, lenka@koskova.cz
 // License: MIT
 
+#include "./gh_definitions.h"
 
-// Greenhouse main internal states
-#define GH_STATE_SLEEP 1              //Device in sleep mode to save energy
-#define GH_STATE_ON 2                 //Device in normal operation 
-#define GH_STATE_UI 3                 //Device interacting with user
-#define GH_STATE_INIT 4               //Device is initializing
-#define GH_STATE_CLOSING 5            //Device is switching off, closing connections etc. 
-#define GH_STATE_ERROR 6              //Device faces some error; restart needed
-
-#define GH_ACT_IN_TEMP 1
-#define GH_ACT_IN_HUMIDITY 2
-#define GH_ACT_OUT_WATER 3
-#define GH_ACT_OUT_DOOR 4
-#define GH_ACT_TELEMETRY 5
-#define GH_ACT_UI_LOCAL 6
-#define GH_ACT_UI_BT 7
-#define GH_ACT_UI_NET 8
-#define GH_ACT_CLOUD 9
-
-#define GH_DELAY_SHORT 1000
-#define GH_DELAY_LONG 3000
-
-#define GH_ERR_NOERR 0 //No error detected, normal operation
-#define GH_ERR_NOINIT 1 //Device starting init in wrong state
-#define GH_ERR_UNKNOWN_STATE 2 //Device in undefined state (restart needed).
-
-
-#define GH_DEBUG_OFF 0 //No debug prints
-#define GH_DEBUG_ON 1 // Debug levels
-#define GH_DEBUG_STRING "GH DEBUG: "
 
 
 int gh_state=GH_STATE_INIT;
@@ -43,27 +15,36 @@ int gh_restart = 0;
 
 
 int gh_process_error(){
-  int ret_code = 0;
+  String to_print="";
+  int ret_code = 0;  
   //Returns 1 if restart is needed.
   switch(gh_err_code) {
     case GH_ERR_NOINIT:
-      Serial.printf("Device in wrong state at init, restarting.\n");
+      to_print=GH_ERR_NOINIT_STR;      
       gh_state=GH_STATE_CLOSING;
       ret_code = 1;
       break;
     case GH_ERR_UNKNOWN_STATE: 
-      Serial.printf("Device in undefined state, restarting.\n");
+      to_print = GH_ERR_UNKNOWN_STATE_STR;
+;            
+      gh_state=GH_STATE_CLOSING;
+      ret_code=1;
+      break;
+    case GH_ERR_MAX_COUNT:
+      to_print = GH_ERR_MAX_COUNT_STR;
       gh_state=GH_STATE_CLOSING;
       ret_code=1;
       break;
     default:
-      Serial.printf("Unsupported err code, back to normal operation.\n");
+      to_print = GH_ERR_NOERR_STR;
       gh_state=GH_STATE_ON;
       ret_code = 0;
       break;
   }
+  Serial.print(to_print);
   return ret_code;
 } 
+
 void print_debug(String to_print) {
   if (gh_debug == GH_DEBUG_ON) {
     // Use %s as a placeholder for a string
@@ -71,13 +52,63 @@ void print_debug(String to_print) {
     Serial.print(full_print);
   }
 }
+
+
+void gh_check_loop() {
+    Serial.printf("GH main loop nr: %d\n", gh_loop_count);
+    gh_loop_count++;
+    if (gh_loop_count > GH_MAX_LOOP) {
+      gh_state = GH_STATE_ERROR;
+      gh_err_code = GH_ERR_MAX_COUNT;
+    }
+
+}
+
+void gh_do_init(){
+
+}
+
+void gh_do_normal_operation() {
+  // Nothing yet, just enering the sleep:
+  print_debug("State: Normal operation\n");
+  gh_state = GH_STATE_SLEEP;
+
+}
+
+void gh_do_sleep() {
+  // No real sleep yet, just delay and back to normal
+  print_debug("State: Sleeping\n");
+  delay(GH_DELAY_LONG);
+  gh_state = GH_STATE_ON;
+
+}
+
+void gh_do_ui() {
+  print_debug("State: User interaction\n");
+  //No UI yet, delay and back to normal operation:
+  delay(GH_DELAY_SHORT);
+  gh_state=GH_STATE_ON;
+}
+
+void gh_do_closing() {
+  print_debug("State: The device is swiching off.\n");
+        // Code to close what is open here:
+  delay(GH_DELAY_SHORT);
+  NVIC_SystemReset();        //Reset device
+}
+
 void setup() {
   //Serial line initialization:
   if (gh_state == GH_STATE_INIT) {
+    
   Serial.begin(9600);
-  Serial.printf("Starting...;\n");
-  Serial.printf("Initialization done.\n");
-  delay(GH_DELAY_LONG);
+  delay(GH_DELAY_SHORT);
+  Serial.print("-------------------------\n");
+  Serial.print("Greenhouse is starting.\n");
+  gh_do_init();
+  Serial.print("Initialization done.\n");
+  Serial.print("-------------------------\n");
+  
   gh_state = GH_STATE_ON;
   }
     else {
@@ -91,33 +122,30 @@ void setup() {
 void loop() {
 
   while (true) { //Main while loop:
-    Serial.printf("GH main loop nr: %d\n", gh_loop_count);
-    gh_loop_count++;
+    gh_check_loop();
+
     // The main loop code:
     // Case state of the device:
     switch(gh_state) {
-      case GH_STATE_ON:
-        print_debug("State: Normal operation\n");
+      case GH_STATE_ON:        
+        gh_do_normal_operation();
         break;
 
-      case(GH_STATE_SLEEP):
-        print_debug("State: Sleeping\n");
+      case(GH_STATE_SLEEP):        
+        gh_do_sleep();
         break;
 
-      case (GH_STATE_UI):
-        print_debug("State: User interaction\n");
+      case (GH_STATE_UI):        
+        gh_do_ui();
         break;
 
       case (GH_STATE_CLOSING):
-        print_debug("State: The device is swiching off.\n");
-        // Code to close what is open here:
-
-        delay(GH_DELAY_SHORT);
-        NVIC_SystemReset();        
+        gh_do_closing();        
         break;
 
       case (GH_STATE_ERROR):
         print_debug("State: The device is in error state.\n");
+        
         //Process the error and if returns 1, restart the device:
         if (gh_process_error()) {
           NVIC_SystemReset();
@@ -130,7 +158,7 @@ void loop() {
         gh_err_code = GH_ERR_UNKNOWN_STATE;
         break;      
       
-    }
+    }    
     delay(GH_DELAY_LONG);
   }
 
